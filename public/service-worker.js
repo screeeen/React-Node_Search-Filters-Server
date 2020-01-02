49bdb61394e7a8f41b6969bd79cd4304ec5d4487
screeeen <miguel@screenimplosion.com>
2020-01-02 10:37:44 +0100

    with map

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ca0165fe5a81bf44b97c40cf52d1c50.js"
+  "/precache-manifest.9309bf9a83f183a1fd23fab9b0497c65.js"
 );
 
 self.addEventListener('message', (event) => {
