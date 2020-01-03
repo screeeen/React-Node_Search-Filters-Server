9ddc3eca9fbee4c5a3b1ba6ba6a5f8c92b63399f
screeeen <miguel@screenimplosion.com>
2020-01-03 12:19:55 +0100

    fixing heroku attempt

---
+++ public/precache-manifest.9309bf9a83f183a1fd23fab9b0497c65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094f066849a6f001647267834691bde6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd3dbd6622e2e8e530f4",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "314ab75af307ffcc037f",
+    "url": "/static/css/main.849fe125.chunk.css"
+  },
+  {
+    "revision": "dd3dbd6622e2e8e530f4",
+    "url": "/static/js/2.74787bc4.chunk.js"
+  },
+  {
+    "revision": "818d9a2a24e8ee6d8e4d77795daa95cd",
+    "url": "/static/js/2.74787bc4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "314ab75af307ffcc037f",
+    "url": "/static/js/main.c86db139.chunk.js"
+  },
+  {
+    "revision": "f71cfe52e06ae7ccf879",
+    "url": "/static/js/runtime-main.b60bb619.js"
+  }
+]);
